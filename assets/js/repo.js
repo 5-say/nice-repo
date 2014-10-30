d8e46972f185bfb7d281958f09ddec9644d95226
5-say <bcw.5@foxmail.com>
2014-10-30 23:40:07 +0800

    update

--- assets/js/repo.js
+++ assets/js/repo.js
@@ -1,16 +1,78 @@
 var repoList =
 [
     {
-        link: 'www.baidu.com',
-        name: '测试',
-        description: 'cheshi'
+        name: '5-say/nice-repo',
+        link: 'https://github.com/5-say/nice-repo',
+        description: '搜集优秀的开源项目。'
     },
     {
-        name: 'Jonas Arnklint',
-        description: '1985'
+        name: 'overtrue/pinyin',
+        link: 'https://github.com/overtrue/pinyin',
+        description: 'Composer 资源包，基于词库的中文转拼音优质解决方案。'
     },
     {
-        name: 'Martina Elm',
-        description: '1986'
+        name: 'robmorgan/phinx',
+        link: 'https://github.com/robmorgan/phinx',
+        description: 'Composer 资源包，通用 PHP 数据库迁移插件。'
+    },
+    {
+        name: 'ruckus/ruckusing-migrations',
+        link: 'https://github.com/ruckus/ruckusing-migrations',
+        description: 'Composer 资源包，通用 PHP 数据库迁移插件。'
+    },
+    {
+        name: 'hyper-carrot/go_command_tutorial',
+        link: 'https://github.com/hyper-carrot/go_command_tutorial',
+        description: 'Go 命令教程。'
+    },
+    {
+        name: 'acl-translation/acl-chinese',
+        link: 'https://github.com/acl-translation/acl-chinese',
+        description: 'ANSI Common Lisp 中文翻译版。'
+    },
+    {
+        name: 'billie66/TLCL',
+        link: 'https://github.com/billie66/TLCL',
+        description: 'The Linux Command Line 一书的中文版。'
+    },
+    {
+        name: 'Semantic-Org/Semantic-UI-zh',
+        link: 'https://github.com/Semantic-Org/Semantic-UI-zh',
+        description: 'Semantic UI 的汉化文档。'
+    },
+    {
+        name: 'jedfoster/Readmore.js',
+        link: 'https://github.com/jedfoster/Readmore.js',
+        description: 'jQuery 插件，用 "Read more" 和 "Close" 链接展开和关闭大段文字。'
+    },
+    {
+        name: 'sofish/imageXSS.js',
+        link: 'https://github.com/sofish/imageXSS.js',
+        description: 'jQuery 插件，防止外部链接通过图片进行 XSS 攻击。'
+    },
+    {
+        name: 'xdan/datetimepicker',
+        link: 'https://github.com/xdan/datetimepicker',
+        description: 'jQuery 插件，时间与日期选择。'
+    },
+    {
+        name: 'boblemarin/jQuery.eraser',
+        link: 'https://github.com/boblemarin/jQuery.eraser',
+        description: 'jQuery 插件，图片擦除效果。'
+    },
+    {
+        name: 'addywaddy/jquery.tagcloud.js',
+        link: 'https://github.com/addywaddy/jquery.tagcloud.js',
+        description: 'jQuery 插件，标签云。'
+    },
+    {
+        name: 'xoxco/jQuery-Tags-Input',
+        link: 'https://github.com/xoxco/jQuery-Tags-Input',
+        description: 'jQuery 插件，标签输入框。'
+    },
+    {
+        name: 'dilumdarshana/Laravel_config',
+        link: 'https://github.com/dilumdarshana/Laravel_config',
+        description: 'Laravel 在 Nginx 下的配置参考。'
     }
 ];
--- md/LaravelMore.md
+++
@@ -1,9 +0,0 @@
-# Laravel 相关
-
----
-
- repo | description
- -----|-------------
- [dilumdarshana/Laravel_config](https://github.com/dilumdarshana/Laravel_config) | Nginx 下的配置参考。
- []() | 
- 
