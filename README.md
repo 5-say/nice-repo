012a6a531a8f333dec4267fbdea8f7200b242e78
5-say <bcw.5@foxmail.com>
2014-10-31 00:14:12 +0800

    update

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-![logo](/assets/img/logo.jpg "nice-repo")
+![logo](/assets/img/logo.png "nice-repo")
 =========
 
-- [Js 插件](/md/JsPlugin.md)
-- [jQuery 插件](/md/jQueryPlugin.md)
-- [Composer 资源包](/md/ComposerPackage.md)
-- [Laravel 资源包](/md/LaravelPackage.md)
-- [Laravel Demo](/md/LaravelDemo.md)
-- [Laravel 相关](/md/LaravelMore.md)
-- [中文文档](/md/DocCn.md)
-- [其它](/md/More.md)
+搜集优秀的开源项目。
+
+您可以编辑 [repo.js](/assets/js/repo.js) 文件，对本项目进行补充。
+
+```json
+    {
+        name: '项目名称',
+        link: '项目链接',
+        description: '项目简介'
+    }
+```
 
 next:
 weather-icons
--- assets/js/repo.js
+++ assets/js/repo.js
@@ -6,6 +6,21 @@ var repoList =
         description: '搜集优秀的开源项目。'
     },
     {
+        name: 'greatfire/wiki',
+        link: 'https://github.com/greatfire/wiki',
+        description: '自由微博 和 GreatFire.org。'
+    },
+    {
+        name: 'aziz/tmTheme-Editor',
+        link: 'https://github.com/aziz/tmTheme-Editor',
+        description: 'Sublime Text 配色方案自定义。'
+    },
+    {
+        name: 'sparanoid/chinese-copywriting-guidelines',
+        link: 'https://github.com/sparanoid/chinese-copywriting-guidelines',
+        description: '中文文案排版指北：统一中文文案、排版的相关用法，降低团队成员之间的沟通成本，增强网站气质。'
+    },
+    {
         name: 'overtrue/pinyin',
         link: 'https://github.com/overtrue/pinyin',
         description: 'Composer 资源包，基于词库的中文转拼音优质解决方案。'
@@ -74,5 +89,110 @@ var repoList =
         name: 'dilumdarshana/Laravel_config',
         link: 'https://github.com/dilumdarshana/Laravel_config',
         description: 'Laravel 在 Nginx 下的配置参考。'
+    },
+    {
+        name: 'sofish/pen',
+        link: 'https://github.com/sofish/pen',
+        description: 'js 插件，MarkDown 在线编辑器。'
+    },
+    {
+        name: 'sofish/wechat.js',
+        link: 'https://github.com/sofish/wechat.js',
+        description: 'js 插件，微信相关的 js 操作：分享、网络、菜单。'
+    },
+    {
+        name: 'RubaXa/Sortable',
+        link: 'https://github.com/RubaXa/Sortable',
+        description: 'js 插件，支持现代浏览器和触摸设备的拖放排序插件。'
+    },
+    {
+        name: 'aui/artTemplate',
+        link: 'https://github.com/aui/artTemplate',
+        description: 'js 插件，性能卓越的 js 模板引擎。'
+    },
+    {
+        name: 'fabien-d/alertify.js',
+        link: 'https://github.com/fabien-d/alertify.js',
+        description: 'js 插件，JavaScript 警告/通知 系统。'
+    },
+    {
+        name: 'zxlie/WeixinApi',
+        link: 'https://github.com/zxlie/WeixinApi',
+        description: 'js 插件，专门用于微信公众平台的Javascript API。'
+    },
+    {
+        name: 'ecomfe/echarts',
+        link: 'https://github.com/ecomfe/echarts',
+        description: 'js 插件，基于Canvas，纯Javascript图表库。'
+    },
+    {
+        name: 'fex-team/webuploader',
+        link: 'https://github.com/fex-team/webuploader',
+        description: 'js 插件，一个简单的以HTML5为主，FLASH为辅的现代文件上传组件。在现代的浏览器里面能充分发挥HTML5的优势，同时又不摒弃主流IE浏览器，延用原来的FLASH运行时，兼容IE6+，Andorid 4+，IOS 6+。'
+    },
+    {
+        name: 'yahoo/gifshot',
+        link: 'https://github.com/yahoo/gifshot',
+        description: 'js 插件，创建流媒体，视频或图像的GIF动画。'
+    },
+    {
+        name: '1000ch/lazyload-image',
+        link: 'https://github.com/1000ch/lazyload-image',
+        description: 'js 插件，图片延迟加载。'
+    },
+    {
+        name: 'nnnick/Chart.js',
+        link: 'https://github.com/nnnick/Chart.js',
+        description: 'js 插件，HTML5 图表库。'
+    },
+    {
+        name: 'rydurham/L4withSentry',
+        link: 'https://github.com/rydurham/L4withSentry',
+        description: 'Laravel Demo，在 Laravel 中使用 Sentry 的 Demo。'
+    },
+    {
+        name: 'octobercms/october',
+        link: 'https://github.com/octobercms/october',
+        description: 'Laravel Demo，基于 Laravel 的 CMS。'
+    },
+    {
+        name: 'sdebacker/TypiCMS',
+        link: 'https://github.com/sdebacker/TypiCMS',
+        description: 'Laravel Demo，基于 Laravel 4.2 的 CMS。'
+    },
+    {
+        name: 'summerblue/phphub',
+        link: 'https://github.com/summerblue/phphub',
+        description: 'Laravel Demo，基于 Laravel 4.2 的社区源码。'
+    },
+    {
+        name: 'lucadegasperi/oauth2-server-laravel',
+        link: 'https://github.com/lucadegasperi/oauth2-server-laravel',
+        description: 'Laravel 资源包，OAuth 2.0。'
+    },
+    {
+        name: 'barryvdh/laravel-cors',
+        link: 'https://github.com/barryvdh/laravel-cors',
+        description: 'Laravel 资源包，跨域资源共享。'
+    },
+    {
+        name: 'LiXunGuan/qiniu',
+        link: 'https://github.com/LiXunGuan/qiniu',
+        description: 'Laravel 资源包，七牛云存储 sdk。<a target="_bland" href="https://github.com/qiniu/php-sdk">原始 sdk</a>。'
+    },
+    {
+        name: 'huanghua581/laravel-wechat-sdk',
+        link: 'https://github.com/huanghua581/laravel-wechat-sdk',
+        description: 'Laravel 资源包，微信公众平台 Laravel SDK。'
+    },
+    {
+        name: 'Maatwebsite/Laravel-Excel',
+        link: 'https://github.com/Maatwebsite/Laravel-Excel',
+        description: 'Laravel 资源包，读取和写入电子表格文件。基于 <a target="_bland" href="https://github.com/PHPOffice/PHPExcel">PHPOffice/PHPExcel</a>。'
+    },
+    {
+        name: '',
+        link: '',
+        description: ''
     }
 ];
